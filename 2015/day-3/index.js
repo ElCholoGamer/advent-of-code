53fed377f3011098fff316d267a57473db246a04
ElCholoGamer <josedanielgrayson@gmail.com>
2020-12-05 16:42:03 -0500

    2015 - day 3

--- 2015/day-3/index.js
+++ 2015/day-3/index.js
@@ -1,1 +1,80 @@
-const input = require('./input.json');
+const [input] = require('./input.json');
+const chars = input.split('');
+
+// Part 1
+const alreadyVisited = [[0, 0]];
+const pos = [0, 0];
+
+const visited = chars.reduce((acc, char, index) => {
+	// Change respective coordinate
+	switch (char) {
+		case '>':
+			pos[0]++;
+			break;
+		case '<':
+			pos[0]--;
+			break;
+		case '^':
+			pos[1]--;
+			break;
+		case 'v':
+			pos[1]++;
+			break;
+		default:
+			throw new Error(`Invalid movement character at index ${index}`);
+	}
+
+	// Check if coordinates were already visited
+	const already = alreadyVisited.some(([x, y]) => x === pos[0] && y === pos[1]);
+
+	if (!already) {
+		alreadyVisited.push([...pos]);
+		return acc + 1;
+	} else {
+		return acc;
+	}
+}, 1);
+
+console.log('Part 1:', visited);
+
+// Part 2
+// Clear visited houses
+alreadyVisited.splice(0, alreadyVisited.length);
+alreadyVisited.push([0, 0]);
+
+const santa = [0, 0];
+const roboSanta = [0, 0];
+
+const newVisited = chars.reduce((acc, char, index) => {
+	// Choose which position to move
+	const pos = index % 2 ? roboSanta : santa;
+
+	// Change respective coordinate
+	switch (char) {
+		case '>':
+			pos[0]++;
+			break;
+		case '<':
+			pos[0]--;
+			break;
+		case '^':
+			pos[1]--;
+			break;
+		case 'v':
+			pos[1]++;
+			break;
+		default:
+			throw new Error(`Invalid movement character at index ${index}`);
+	}
+
+	// Check if coordinates were already visited
+	const already = alreadyVisited.some(([x, y]) => x === pos[0] && y === pos[1]);
+	if (!already) {
+		alreadyVisited.push([...pos]);
+		return acc + 1;
+	} else {
+		return acc;
+	}
+}, 1);
+
+console.log('Part 2:', newVisited);
