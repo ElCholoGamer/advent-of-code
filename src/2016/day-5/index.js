f18c93bd11d6d4106ecf73e9789c7bd42d76d4d4
ElCholoGamer <josedanielgrayson@gmail.com>
2020-12-06 17:19:28 -0500

    2016 - day 5 (Use --cinematic flag for something cool)

--- src/2016/day-5/index.js
+++ src/2016/day-5/index.js
@@ -1,1 +1,38 @@
-const input = require('./input.json');
+const md5 = require('md5');
+const chalk = require('chalk');
+const [input] = require('./input.json');
+
+// Part 1
+let password = '';
+let count = 0;
+
+for (let i = 0; i < 8; i++) {
+	let hash;
+
+	// while (!(hash = md5(input + (count += 1))).startsWith('00000')) {}
+	// password += hash[5];
+}
+
+console.log('Part 1:', password);
+
+// Part 2
+count = 0;
+password = '-'.repeat(8);
+
+const cinematic = process.argv.includes('--cinematic');
+while (password.indexOf('-') !== -1) {
+	let hash;
+	while (!(hash = md5(input + (count += 1))).startsWith('00000')) {
+		if (cinematic && count % 20000 === 0)
+			console.log(chalk.greenBright(`${hash} | Password: ${password}`));
+	}
+
+	const index = parseInt(hash[5]);
+	if (password[index] !== '-') continue;
+
+	const value = hash[6];
+	password = password.substr(0, index) + value + password.substr(index + 1);
+}
+
+if (cinematic) console.log(chalk.green('[ Password decrypted ]'));
+console.log('Part 2:', password);
