c0c4297d073443fd549267eaf490769f9f305d7d
ElCholoGamer <josedanielgrayson@gmail.com>
2020-12-08 00:20:56 -0500

    2016 - day 9 (Part 1)

--- src/2016/day-9/index.js
+++ src/2016/day-9/index.js
@@ -1,1 +1,52 @@
-const input = require('./input.json');
+const [input] = require('./input.json');
+
+// Part 1
+let decompressed = input;
+const regex = /(?<!\([0-9]+x[0-9]+\))(\([0-9]+x[0-9]+\))/g;
+
+let match;
+while ((match = regex.exec(decompressed)) !== null) {
+	const { index } = match;
+	const [length, times] = match[0]
+		.match(/[0-9]+/g)
+		.map(match => parseInt(match));
+
+	const str = decompressed.substr(index + match[0].length, length);
+
+	// Add a space to these to prevent regex matching
+	const repeated = str.replace(/\(/g, '( ').repeat(times);
+
+	decompressed =
+		decompressed.substr(0, index) +
+		repeated +
+		decompressed.substr(index + match[0].length + str.length);
+}
+
+console.log('Part 1:', decompressed.replace(/\s+/g, '').length);
+
+// Part 2
+function decompress(string) {
+	const regex = /(?<!\([0-9]+x[0-9]+\))(\([0-9]+x[0-9]+\))/g;
+
+	let match;
+	while ((match = regex.exec(string)) !== null) {
+		const { index } = match;
+		const [length, times] = match[0]
+			.match(/[0-9]+/g)
+			.map(match => parseInt(match));
+
+		let sub = string.substr(index + match[0].length, length);
+		sub = decompress(sub);
+
+		string =
+			string.substr(0, index) +
+			sub.repeat(times) +
+			string.substr(index + match[0].length + length);
+	}
+
+	return string;
+}
+
+decompressed = decompress(input);
+console.log(decompressed);
+console.log('Part 2:', decompressed.length);
