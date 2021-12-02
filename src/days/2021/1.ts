f1b821d13fff0c35e024e41df21ca2e31db91109
ElCholoGamer <josedanielgrayson@gmail.com>
2021-12-01 21:11:30 -0500

    2020 - day 1

--- src/days/2021/1.ts
+++ src/days/2021/1.ts
@@ -1,9 +1,33 @@
 import { AoCPart } from '../../types';
 
-export const part1: AoCPart = input => {
-	return '[Part 1 answer]';
+export const part1: AoCPart = inputStrings => {
+	const input = inputStrings.map(Number);
+
+	let increments = 0;
+
+	for (let i = 1; i < input.length; i++) {
+		increments += +(input[i] > input[i - 1]);
+	}
+
+	return increments;
 };
 
-export const part2: AoCPart = input => {
-	return '[Part 2 answer]';
+export const part2: AoCPart = inputStrings => {
+	const input = inputStrings.map(Number);
+	const windows = [];
+
+	for (let i = 0; i < input.length; i += 4) {
+		windows.push(input[i] + input[i + 1] + input[i + 2]);
+	}
+
+	let increments = 0;
+
+	for (let i = 1; i < input.length; i++) {
+		const window = input[i] + input[i + 1] + input[i + 2];
+		const prevWindow = input[i - 1] + input[i] + input[i + 1];
+
+		increments += +(window > prevWindow);
+	}
+
+	return increments;
 };
