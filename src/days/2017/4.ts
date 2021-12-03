b0162bf56e3f724521b66a3e0fdab0f395ea0de3
ElCholoGamer <josedanielgrayson@gmail.com>
2021-12-02 20:26:36 -0500

    2017 - day 4

---
+++ src/days/2017/4.ts
@@ -0,0 +1,41 @@
+import { AoCPart } from '../../types';
+
+function replaceAt(str: string, index: number, replacement: string) {
+	return str.substr(0, index) + replacement + str.substr(index + 1, str.length);
+}
+
+export const part1: AoCPart = input => {
+	let validPassphrases = 0;
+
+	for (const line of input) {
+		const words = line.split(/\s+/);
+
+		if (words.every((word, index) => words.indexOf(word) === index)) {
+			validPassphrases++;
+		}
+	}
+
+	return validPassphrases;
+};
+
+export const part2: AoCPart = input => {
+	let validPassphrases = 0;
+
+	for (const line of input) {
+		const words = line.split(/\s+/);
+
+		validPassphrases += +words.every((word, index) => {
+			const sortedWord = word.split('').sort().join('');
+			for (let i = 0; i < words.length; i++) {
+				if (i === index || words[i].length !== word.length) continue;
+
+				const sortedOtherWord = words[i].split('').sort().join('');
+				if (sortedWord === sortedOtherWord) return false;
+			}
+
+			return true;
+		});
+	}
+
+	return validPassphrases;
+};
