4b7cd028649441f56a4997e5a2095a36ed290b2f
ElCholoGamer <josedanielgrayson@gmail.com>
2021-12-06 11:55:11 -0500

    2018 - day 1

---
+++ src/days/2018/1.ts
@@ -0,0 +1,24 @@
+import { AoCPart } from '../../types';
+
+export const part1: AoCPart = input => {
+	return input.map(Number).reduce((a, b) => a + b);
+};
+
+export const part2: AoCPart = input => {
+	const seenFrequencies = new Set<number>();
+	const changes = input.map(Number);
+
+	let index = 0;
+	let freq = 0;
+
+	while (true) {
+		freq += changes[index];
+
+		if (seenFrequencies.has(freq)) break;
+
+		seenFrequencies.add(freq);
+		index = ++index % changes.length;
+	}
+
+	return freq;
+};
