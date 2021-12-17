9fbf8d8d173485f3b3b03646ce2e08156c5586d3
ElCholoGamer <josedanielgrayson@gmail.com>
2021-12-16 23:08:11 -0500

    2017 - day 10

---
+++ src/days/2017/10.ts
@@ -0,0 +1,58 @@
+import { AoCPart } from '../../types';
+
+function reverseSelection<T>(arr: T[], position: number, length: number): T[] {
+	const out = [...arr];
+
+	for (let i = 0; i < length; i++) {
+		const to = (position + i) % arr.length;
+		const from = (position + length - 1 - i) % arr.length;
+
+		arr[to] = arr[from];
+	}
+
+	return out;
+}
+
+export const part1: AoCPart = ([input]) => {
+	const lengths = input.split(',').map(Number);
+	let marks = [...Array(256)].map((_, index) => index);
+
+	let skipSize = 0;
+	let position = 0;
+
+	for (const length of lengths) {
+		marks = reverseSelection(marks, position, length);
+		position = (position + length + skipSize) % marks.length;
+		skipSize++;
+	}
+
+	return marks[0] * marks[1];
+};
+
+export const part2: AoCPart = ([input]) => {
+	let marks = [...Array(256)].map((_, index) => index);
+
+	const lengths = input.split('').map(char => char.charCodeAt(0));
+	lengths.push(17, 31, 73, 47, 23);
+
+	let skipSize = 0;
+	let position = 0;
+
+	for (let round = 0; round < 64; round++) {
+		for (const length of lengths) {
+			marks = reverseSelection(marks, position, length);
+			position = (position + length + skipSize) % marks.length;
+			skipSize++;
+		}
+	}
+
+	const denseHash: number[] = [];
+
+	for (let i = 0; i < marks.length; i += 16) {
+		const block = marks.slice(i, i + 16);
+		denseHash.push(block.reduce((a, b) => a ^ b));
+	}
+
+	const hexChars = denseHash.map(num => num.toString(16).padStart(2, '0'));
+	return hexChars.join('');
+};
