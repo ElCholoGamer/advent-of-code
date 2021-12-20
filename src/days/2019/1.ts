e12f828f0e01430e46950cf847b124b223fa1122
ElCholoGamer <josedanielgrayson@gmail.com>
2021-12-19 23:24:22 -0500

    2019 - day 1

---
+++ src/days/2019/1.ts
@@ -0,0 +1,26 @@
+import { AoCPart } from '../../types';
+
+export const part1: AoCPart = input => {
+	const masses = input.map(Number);
+	const fuelRequirements = masses.map(mass => Math.floor(mass / 3) - 2);
+
+	return fuelRequirements.reduce((a, b) => a + b);
+};
+
+export const part2: AoCPart = input => {
+	const masses = input.map(Number);
+
+	let totalFuel = 0;
+	const stack: number[] = [...masses];
+
+	while (stack.length > 0) {
+		const mass = stack.pop()!;
+		const fuelRequirement = Math.floor(mass / 3) - 2;
+		if (fuelRequirement <= 0) continue;
+
+		totalFuel += fuelRequirement;
+		stack.push(fuelRequirement);
+	}
+
+	return totalFuel;
+};
