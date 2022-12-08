98d0b710475a44e7d7aa05df08bf43bf5c3403c5
ElCholoGamer <josedanielgrayson@gmail.com>
2022-12-08 07:57:43 -0500

    refactor 2022 day 8

--- src/days/2022/8.ts
+++ src/days/2022/8.ts
@@ -6,61 +6,58 @@ interface Tree {
 	markedVisible: boolean;
 }
 
-function parseInput(input: string[]): Tree[][] {
-	return [...Array(input[0].length)].map((_, x) =>
-		input.map(line => ({
-			height: parseInt(line[x]),
-			markedVisible: false,
-		}))
-	);
+function parseTreeHeights(input: string[]): number[][] {
+	return [...Array(input[0].length)].map((_, x) => input.map(line => parseInt(line[x])));
 }
 
 export const part1: AoCPart = input => {
-	const trees = parseInput(input);
+	const trees: Tree[][] = parseTreeHeights(input).map(col =>
+		col.map(height => ({ height, markedVisible: false }))
+	);
 
-	for (let y = 0; y < trees.length; y++) {
+	for (let x = 0; x < trees.length; x++) {
 		let maxHeight = -1;
 
-		for (let x = 0; x < trees[y].length; x++) {
-			if (trees[y][x].height > maxHeight) {
-				trees[y][x].markedVisible = true;
+		for (let y = 0; y < trees[x].length; y++) {
+			if (trees[x][y].height > maxHeight) {
+				trees[x][y].markedVisible = true;
 
-				maxHeight = trees[y][x].height;
+				maxHeight = trees[x][y].height;
 				if (maxHeight === 9) break;
 			}
 		}
 
 		maxHeight = -1;
 
-		for (let x = trees[y].length - 1; x >= 0; x--) {
-			if (trees[y][x].height > maxHeight) {
-				trees[y][x].markedVisible = true;
+		for (let y = trees[x].length - 1; y >= 0; y--) {
+			if (trees[x][y].height > maxHeight) {
+				trees[x][y].markedVisible = true;
 
-				maxHeight = trees[y][x].height;
+				maxHeight = trees[x][y].height;
 				if (maxHeight === 9) break;
 			}
 		}
 	}
 
-	for (let x = 0; x < trees[0].length; x++) {
+	for (let y = 0; y < trees[0].length; y++) {
 		let maxHeight = -1;
 
-		for (let y = 0; y < trees.length; y++) {
-			if (trees[y][x].height > maxHeight) {
-				trees[y][x].markedVisible = true;
+		for (let x = 0; x < trees.length; x++) {
+			if (trees[x][y].height > maxHeight) {
+				trees[x][y].markedVisible = true;
 
-				maxHeight = trees[y][x].height;
+				maxHeight = trees[x][y].height;
 				if (maxHeight === 9) break;
 			}
 		}
 
 		maxHeight = -1;
 
-		for (let y = trees.length - 1; y >= 0; y--) {
-			if (trees[y][x].height > maxHeight) {
-				trees[y][x].markedVisible = true;
+		for (let x = trees.length - 1; x >= 0; x--) {
+			if (trees[x][y].height > maxHeight) {
+				trees[x][y].markedVisible = true;
 
-				maxHeight = trees[y][x].height;
+				maxHeight = trees[x][y].height;
 				if (maxHeight === 9) break;
 			}
 		}
@@ -69,30 +66,23 @@ export const part1: AoCPart = input => {
 	return trees.reduce((sum, row) => sum + count(row, tree => tree.markedVisible), 0);
 };
 
-interface ViewingTree extends Tree {
-	score: number;
-}
-
 export const part2: AoCPart = input => {
-	const trees: ViewingTree[][] = parseInput(input).map(col =>
-		col.map(tree => ({ ...tree, score: -1 }))
-	);
+	const treeHeights = parseTreeHeights(input);
 
-	function viewingDistanceX(treeX: number, treeY: number, step: 1 | -1): number {
+	function viewingDistance(treeX: number, treeY: number, axis: 'x' | 'y', step: 1 | -1): number {
+		const baseHeight = treeHeights[treeX][treeY];
 		let viewingDistance = 0;
-		for (let x = treeX + step; x < trees[treeY].length && x >= 0; x += step) {
-			viewingDistance++;
-			if (trees[treeY][x].height >= trees[treeY][treeX].height) break;
-		}
 
-		return viewingDistance;
-	}
-
-	function viewingDistanceY(treeX: number, treeY: number, step: 1 | -1): number {
-		let viewingDistance = 0;
-		for (let y = treeY + step; y < trees.length && y >= 0; y += step) {
-			viewingDistance++;
-			if (trees[y][treeX].height >= trees[treeY][treeX].height) break;
+		if (axis === 'x') {
+			for (let x = treeX + step; x < treeHeights.length && x >= 0; x += step) {
+				viewingDistance++;
+				if (treeHeights[x][treeY] >= baseHeight) break;
+			}
+		} else {
+			for (let y = treeY + step; y < treeHeights[0].length && y >= 0; y += step) {
+				viewingDistance++;
+				if (treeHeights[treeX][y] >= baseHeight) break;
+			}
 		}
 
 		return viewingDistance;
@@ -100,12 +90,12 @@ export const part2: AoCPart = input => {
 
 	let maxScore = 0;
 
-	for (let y = 0; y < trees.length; y++) {
-		for (let x = 0; x < trees[y].length; x++) {
-			const up = viewingDistanceY(x, y, -1);
-			const left = viewingDistanceX(x, y, -1);
-			const down = viewingDistanceY(x, y, 1);
-			const right = viewingDistanceX(x, y, 1);
+	for (let x = 0; x < treeHeights.length; x++) {
+		for (let y = 0; y < treeHeights[x].length; y++) {
+			const up = viewingDistance(x, y, 'y', -1);
+			const left = viewingDistance(x, y, 'x', -1);
+			const down = viewingDistance(x, y, 'y', 1);
+			const right = viewingDistance(x, y, 'x', 1);
 
 			const score = up * left * down * right;
 			if (score > maxScore) maxScore = score;
