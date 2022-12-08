d9e363f783a07a073fd74645524baff2362e32bd
ElCholoGamer <josedanielgrayson@gmail.com>
2022-12-07 19:30:02 -0500

    2022 - day 6

---
+++ src/days/2022/6.ts
@@ -0,0 +1,13 @@
+import { AoCPart } from '../../types';
+import { windows } from '../../utils';
+
+function findMarker(str: string, markerLength: number) {
+	return (
+		windows(str.split(''), markerLength).findIndex(seq => new Set(seq).size === seq.length) +
+		markerLength
+	);
+}
+
+export const part1: AoCPart = ([input]) => findMarker(input, 4);
+
+export const part2: AoCPart = ([input]) => findMarker(input, 14);
