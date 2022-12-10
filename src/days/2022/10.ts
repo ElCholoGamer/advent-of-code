af24e6adc342532b9667e6d1e2dcada7d6104a3b
ElCholoGamer <josedanielgrayson@gmail.com>
2022-12-10 15:25:59 -0500

    add visualization for 2022 day 10

--- src/days/2022/10.ts
+++ src/days/2022/10.ts
@@ -1,5 +1,8 @@
-import { AoCPart } from '../../types';
+import chalk from 'chalk';
+import { AoCPart, Visualization } from '../../types';
+import { sleep } from '../../utils';
 import { chunks } from '../../utils/arrays';
+import { HIDE_CURSOR, SHOW_CURSOR } from '../../utils/strings';
 
 const parseInstruction = (str: string) => (str === 'noop' ? null : parseInt(str.split(' ')[1]));
 
@@ -56,4 +59,68 @@ export const part2: AoCPart = input => {
 			.map(lineData => lineData.map(p => (p ? '##' : '  ')).join(''))
 			.join('\n')
 	);
+};
+
+export const visualization: Visualization = async input => {
+	const instructions = input.map(parseInstruction);
+
+	let cycle = 0;
+	let xReg = 1;
+	const pixels = Array<boolean>(240).fill(false);
+
+	function render(instructionIndex: number) {
+		console.clear();
+		const lineChars = chunks(pixels, 40).map((line, y) =>
+			line.map((b, x) => chalk[y * 40 + x === cycle ? 'bgWhite' : b ? 'bgGreen' : 'bgGray']('  '))
+		);
+
+		console.log(chalk.blue`Cycles: ${cycle.toString().padEnd(5)} X: ${xReg.toString()}`);
+		console.log();
+		console.log(
+			[...Array(40)]
+				.map((_, index) => (Math.abs(xReg - index) <= 1 ? chalk.bgYellow('  ') : '  '))
+				.join('')
+		);
+
+		const lines = lineChars.map(row => row.join(''));
+		for (let i = 0; i < lines.length; i++) {
+			const lineInstructionIndex = instructionIndex - 2 + i;
+			if (lineInstructionIndex < 0 || lineInstructionIndex >= instructions.length) continue;
+
+			const maybeAddX = instructions[lineInstructionIndex];
+			let extra = maybeAddX === null ? 'noop' : `addx ${maybeAddX}`;
+
+			if (lineInstructionIndex === instructionIndex) {
+				lines[i] += `   ${chalk.yellow`> ${extra}`}`;
+			} else {
+				lines[i] += `     ${chalk.gray(extra)}`;
+			}
+		}
+
+		console.log(lines.join('\n'));
+	}
+
+	async function nextCycle(instructionIndex: number) {
+		const pixelXPosition = cycle % 40;
+		pixels[cycle] = pixelXPosition >= xReg - 1 && pixelXPosition <= xReg + 1;
+		cycle++;
+
+		render(instructionIndex);
+		await sleep(50);
+	}
+
+	console.log(HIDE_CURSOR);
+	render(-1);
+	await sleep(500);
+
+	for (let i = 0; i < instructions.length; i++) {
+		await nextCycle(i);
+		const maybeAddX = instructions[i];
+		if (maybeAddX !== null) {
+			await nextCycle(i);
+			xReg += maybeAddX;
+		}
+	}
+
+	console.log(SHOW_CURSOR);
 };
