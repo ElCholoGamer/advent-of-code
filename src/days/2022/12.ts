ac0584fcd58d5f800fa27eeff4083e7208034e77
ElCholoGamer <josedanielgrayson@gmail.com>
2022-12-12 17:07:15 -0500

    add visualization for 2022 day 12

--- src/days/2022/12.ts
+++ src/days/2022/12.ts
@@ -1,4 +1,7 @@
-import { AoCPart } from '../../types';
+import chalk from 'chalk';
+import { AoCPart, Visualization } from '../../types';
+import { sleep } from '../../utils';
+import { HIDE_CURSOR, SHOW_CURSOR } from '../../utils/strings';
 import { Vector2 } from '../../utils/vector';
 
 interface Square {
@@ -87,4 +90,108 @@ export const part2: AoCPart = input => {
 
 	const aSquares = grid.flat().filter(square => square.height === 0);
 	return Math.min(...aSquares.map(square => square.steps));
+};
+
+interface TrackedSquare extends Square {
+	neighbors: TrackedSquare[];
+	back: TrackedSquare | null;
+}
+
+export const visualization: Visualization = async input => {
+	const { grid: baseGrid, start, end } = parseMap(input, (h, nH) => nH <= h + 1);
+	const grid = baseGrid as TrackedSquare[][];
+
+	for (const column of grid) {
+		for (const square of column) {
+			square.back = null;
+		}
+	}
+
+	function render(finished: boolean) {
+		const lines = Array(grid[0].length);
+
+		for (let y = 0; y < grid[0].length; y++) {
+			let coloring = false;
+			let line = '';
+
+			for (let x = 0; x < grid.length; x++) {
+				const square = grid[x][y];
+				const char =
+					square === start ? 'S' : square === end ? 'E' : String.fromCharCode(square.height + 97);
+
+				if (square === start || square === end) {
+					line += chalk.bold.blue(char);
+					coloring = false;
+					continue;
+				} else if (finished) {
+					line += square.visited ? chalk.green('#') : chalk.gray(char);
+					continue;
+				} else if (square.visited && !coloring) {
+					line += (chalk.yellow as any)._styler.open;
+					coloring = true;
+				} else if (!square.visited && coloring) {
+					line += (chalk.reset as any)._styler.open;
+					coloring = false;
+				}
+
+				line += char;
+			}
+
+			lines[y] = line + (chalk.reset as any)._styler.open;
+		}
+
+		console.clear();
+		console.log(lines.join('\n'));
+	}
+
+	start.steps = 0;
+	start.visited = true;
+	const queue = [start as TrackedSquare];
+
+	console.log(HIDE_CURSOR);
+	render(false);
+	await sleep(500);
+
+	let level = 0;
+
+	while (queue.length !== 0) {
+		const square = queue.shift()!;
+
+		if (square.steps === level) {
+			render(false);
+			await sleep(20);
+			level++;
+		}
+
+		for (const neighbor of square.neighbors) {
+			if (neighbor.visited) continue;
+
+			neighbor.back = square;
+			neighbor.steps = square.steps + 1;
+			neighbor.visited = true;
+			queue.push(neighbor);
+		}
+
+		if (end.visited) break;
+	}
+
+	for (const column of grid) {
+		for (const square of column) {
+			square.visited = false;
+		}
+	}
+
+	let currentSquare: TrackedSquare | null = end as TrackedSquare;
+	while (currentSquare) {
+		currentSquare.visited = true;
+		currentSquare = currentSquare.back;
+	}
+
+	await sleep(500);
+	render(true);
+	console.log();
+	console.log(chalk.green`${chalk.bold('Shortest distance:')} ${end.steps}`);
+	await sleep(5000);
+
+	console.log(SHOW_CURSOR);
 };
