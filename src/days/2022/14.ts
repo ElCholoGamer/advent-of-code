a760e1a410dc2f718a29aee1d350509fe4bca9a9
ElCholoGamer <josedanielgrayson@gmail.com>
2022-12-14 11:45:35 -0500

    small 2022 day 14 optimization

--- src/days/2022/14.ts
+++ src/days/2022/14.ts
@@ -1,6 +1,7 @@
-import { AoCPart } from '../../types';
-import { ExtendableGrid } from '../../utils/extendable-grid';
+import { AoCPart, Visualization } from '../../types';
 import { Vector2 } from '../../utils/vector';
+
+const SAND_ORIGIN = new Vector2(500, 0);
 
 const enum Tile {
 	AIR,
@@ -8,11 +9,13 @@ const enum Tile {
 	SAND,
 }
 
-function buildGrid(input: string[]) {
+function buildGrid(input: string[]): { grid: Tile[][]; floor: number } {
 	const paths = input.map(line =>
 		line.split(' -> ').map(s => Vector2.fromArray(s.split(',').map(Number)))
 	);
-	const grid = new ExtendableGrid(Tile.AIR);
+	const grid = [...Array(SAND_ORIGIN.x * 2)].map(() => Array(SAND_ORIGIN.x).fill(Tile.AIR));
+
+	let maxRockY = 0;
 
 	for (const path of paths) {
 		for (let i = 0; i < path.length - 1; i++) {
@@ -20,32 +23,34 @@ function buildGrid(input: string[]) {
 			const to = path[i + 1];
 			const step = to.clone().subtract(from).sign();
 
+			maxRockY = Math.max(maxRockY, from.y, to.y);
+
 			while (!from.equals(to)) {
-				grid.set(from.x, from.y, Tile.ROCK);
+				grid[from.x][from.y] = Tile.ROCK;
 				from.add(step);
 			}
-			grid.set(to.x, to.y, Tile.ROCK);
+			grid[to.x][to.y] = Tile.ROCK;
 		}
 	}
 
-	return grid;
+	return { grid, floor: maxRockY + 2 };
 }
 
 export const part1: AoCPart = input => {
-	const grid = buildGrid(input);
+	const { grid, floor } = buildGrid(input);
 
 	for (let sandUnits = 0; ; sandUnits++) {
-		const sandPos = new Vector2(500, 0);
+		const sandPos = SAND_ORIGIN.clone();
 
 		while (true) {
-			if (sandPos.y >= grid.height - 1) return sandUnits;
+			if (sandPos.y >= floor) return sandUnits;
 
 			const nextMove = [0, -1, 1].find(
-				xMove => grid.get(sandPos.x + xMove, sandPos.y + 1) === Tile.AIR
+				xMove => grid[sandPos.x + xMove][sandPos.y + 1] === Tile.AIR
 			);
 
 			if (nextMove === undefined) {
-				grid.set(sandPos.x, sandPos.y, Tile.SAND);
+				grid[sandPos.x][sandPos.y] = Tile.SAND;
 				break;
 			}
 
@@ -56,26 +61,25 @@ export const part1: AoCPart = input => {
 };
 
 export const part2: AoCPart = input => {
-	const grid = buildGrid(input);
-	const floor = grid.height;
+	const { grid, floor } = buildGrid(input);
 
-	for (let sandUnits = 0; ; sandUnits++) {
-		const sandPos = new Vector2(500, 0);
+	for (let sandUnits = 1; ; sandUnits++) {
+		const sandPos = SAND_ORIGIN.clone();
 
 		while (true) {
-			if (sandPos.y === floor) {
-				grid.set(sandPos.x, sandPos.y, Tile.SAND);
+			if (sandPos.y === floor - 1) {
+				grid[sandPos.x][sandPos.y] = Tile.SAND;
 				break;
 			}
 
 			const nextMove = [0, -1, 1].find(
-				xMove => grid.get(sandPos.x + xMove, sandPos.y + 1, Tile.AIR) === Tile.AIR
+				xMove => grid[sandPos.x + xMove][sandPos.y + 1] === Tile.AIR
 			);
 
 			if (nextMove === undefined) {
-				if (sandPos.y === 0) return sandUnits + 1;
+				if (sandPos.y === 0) return sandUnits;
 
-				grid.set(sandPos.x, sandPos.y, Tile.SAND);
+				grid[sandPos.x][sandPos.y] = Tile.SAND;
 				break;
 			}
 
