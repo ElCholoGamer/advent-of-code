385f372cf18fd7457c2b02a34954d0f9e2107d58
ElCholoGamer <josedanielgrayson@gmail.com>
2023-12-02 17:03:12 -0500

    day 2 improvements

--- src/days/2023/2.ts
+++ src/days/2023/2.ts
@@ -1,16 +1,14 @@
 import { AoCPart } from '../../types';
 
+type Color = 'red' | 'green' | 'blue';
+
 interface Game {
 	id: number;
 	picks: Pick[];
 }
 
 interface Pick {
-	entries: Entry[];
-}
-
-interface Entry {
-	color: 'red' | 'green' | 'blue';
+	color: Color;
 	amount: number;
 }
 
@@ -21,18 +19,16 @@ function parseGames(input: string[]) {
 		const [gameLabel, pickStrings] = line.split(': ');
 		const gameId = Number(gameLabel.split(' ')[1]);
 
-		const picks: Pick[] = pickStrings.split('; ').map((pickStr) => {
-			const entryStrings = pickStr.split(', ');
-			return {
-				entries: entryStrings.map((str) => {
-					const words = str.split(' ');
-					return {
-						color: words[1] as any,
-						amount: Number(words[0]),
-					};
-				}),
-			};
-		});
+		const picks: Pick[] = pickStrings
+			.replaceAll(';', ',')
+			.split(', ')
+			.map((str) => {
+				const words = str.split(' ');
+				return {
+					color: words[1] as any,
+					amount: Number(words[0]),
+				};
+			});
 
 		out.push({ id: gameId, picks });
 	}
@@ -40,72 +36,45 @@ function parseGames(input: string[]) {
 	return out;
 }
 
-export const part1: AoCPart = (input) => {
-	let sum = 0;
+function getMaxPickedCubes(game: Game): Record<Color, number> {
+	const out: Record<Color, number> = {
+		red: 0,
+		blue: 0,
+		green: 0,
+	};
 
+	for (const { color, amount } of game.picks) {
+		if (amount > out[color]) out[color] = amount;
+	}
+
+	return out;
+}
+
+export const part1: AoCPart = (input) => {
 	const CUBE_COUNT = {
 		red: 12,
 		green: 13,
 		blue: 14,
 	};
 
-	const games = parseGames(input);
-
-	for (const game of games) {
-		const isGamePossible = game.picks.every((pick) => {
-			const pickedCubes = {
-				red: 0,
-				green: 0,
-				blue: 0,
-			};
-
-			for (const { color, amount } of pick.entries) {
-				pickedCubes[color] += amount;
-				if (pickedCubes[color] > CUBE_COUNT[color]) {
-					return false;
-				}
-			}
-
-			return true;
-		});
-
-		if (isGamePossible) {
-			sum += game.id;
-		}
-	}
-
-	return sum;
+	return parseGames(input)
+		.filter((game) => {
+			const maxPickedCubes = getMaxPickedCubes(game);
+			return (
+				maxPickedCubes.red <= CUBE_COUNT.red &&
+				maxPickedCubes.green <= CUBE_COUNT.green &&
+				maxPickedCubes.blue <= CUBE_COUNT.blue
+			);
+		})
+		.map((game) => game.id)
+		.reduce((a, b) => a + b);
 };
 
 export const part2: AoCPart = (input) => {
-	const games = parseGames(input);
-	let sum = 0;
-
-	for (const game of games) {
-		const minRequiredCubes = {
-			red: 0,
-			green: 0,
-			blue: 0,
-		};
-
-		for (const pick of game.picks) {
-			const pickedCubes = {
-				red: 0,
-				green: 0,
-				blue: 0,
-			};
-
-			for (const { color, amount } of pick.entries) {
-				pickedCubes[color] += amount;
-			}
-
-			minRequiredCubes.red = Math.max(minRequiredCubes.red, pickedCubes.red);
-			minRequiredCubes.green = Math.max(minRequiredCubes.green, pickedCubes.green);
-			minRequiredCubes.blue = Math.max(minRequiredCubes.blue, pickedCubes.blue);
-		}
-
-		sum += minRequiredCubes.red * minRequiredCubes.green * minRequiredCubes.blue;
-	}
-
-	return sum;
+	return parseGames(input)
+		.map((game) => {
+			const maxPickedCubes = getMaxPickedCubes(game);
+			return maxPickedCubes.red * maxPickedCubes.green * maxPickedCubes.blue;
+		})
+		.reduce((a, b) => a + b);
 };
