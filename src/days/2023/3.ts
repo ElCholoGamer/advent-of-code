7cad34519829152e471b445eb12abe74cf3699b3
ElCholoGamer <josedanielgrayson@gmail.com>
2023-12-03 00:39:01 -0500

    2023 - day 3

---
+++ src/days/2023/3.ts
@@ -0,0 +1,95 @@
+import { AoCPart } from '../../types';
+
+const isDigit = (char: string) => !Number.isNaN(Number(char));
+
+const isSymbol = (char: string) => char !== '.' && !isDigit(char);
+
+export const part1: AoCPart = (input) => {
+	let sum = 0;
+
+	for (let l = 0; l < input.length; l++) {
+		const line = input[l];
+
+		for (let i = 0; i < line.length; i++) {
+			const char = line[i];
+			if (!isDigit(char)) continue;
+
+			let end = i;
+			let numStr = '';
+			do {
+				numStr += line[end];
+				end++;
+			} while (end < line.length && isDigit(line[end]));
+
+			let isPartNumber = false;
+
+			for (let col = Math.max(i - 1, 0); col <= Math.min(end, line.length - 1); col++) {
+				if (
+					(l > 0 && isSymbol(input[l - 1][col])) ||
+					isSymbol(input[l][col]) ||
+					(l < input.length - 1 && isSymbol(input[l + 1][col]))
+				) {
+					isPartNumber = true;
+					break;
+				}
+			}
+
+			if (isPartNumber) sum += Number(numStr);
+			i = end;
+		}
+	}
+
+	return sum;
+};
+
+export const part2: AoCPart = (input) => {
+	function searchNumberFrom(line: number, column: number): number {
+		while (column > 0 && isDigit(input[line][column - 1])) {
+			column--;
+		}
+
+		let numStr = '';
+
+		do {
+			numStr += input[line][column];
+			column++;
+		} while (column < input[line].length && isDigit(input[line][column]));
+
+		return Number(numStr);
+	}
+
+	let sum = 0;
+
+	for (let l = 0; l < input.length; l++) {
+		const line = input[l];
+
+		for (let c = 0; c < line.length; c++) {
+			if (line[c] !== '*') continue;
+
+			const numbers = new Set<number>();
+
+			function tryGearNumber(line: number, column: number) {
+				if (isDigit(input[line]?.[column])) {
+					numbers.add(searchNumberFrom(line, column));
+				}
+			}
+
+			for (let lo = -1; lo <= 1; lo++) {
+				for (let co = -1; co <= 1; co++) {
+					if (lo === 0 && co === 0) continue;
+
+					if (isDigit(input[l + lo]?.[c + co])) {
+						numbers.add(searchNumberFrom(l + lo, c + co));
+					}
+				}
+			}
+
+			if (numbers.size === 2) {
+				const numArray = Array.from(numbers.values());
+				sum += numArray[0] * numArray[1];
+			}
+		}
+	}
+
+	return sum;
+};
